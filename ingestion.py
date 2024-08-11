b72f8e9185f28629df7e83a7a25c71574a6c9f53
D Vaillant <david@vaillant.io>
2024-08-11 17:24:03 -0400

    Ingestion works. Generates embeddings.

--- ingestion.py
+++ ingestion.py
@@ -1,159 +1,36 @@
 # reads the friends corpus and returns it as nodes.
-from collections import deque
-from pathlib import Path
 import json
-from typing import Dict, List, Optional
-from fsspec import AbstractFileSystem
+from pathlib import Path
+from typing import Dict, List
 
-from llama_index.core import SimpleDirectoryReader
-# from llama_index.core.readers import JSONReader
-from llama_index.core.node_parser import JSONNodeParser
 from llama_index.core import VectorStoreIndex, StorageContext
-from llama_index.core.schema import TextNode, NodeRelationship, RelatedNodeInfo, Document
-from llama_index.core.readers.base import BaseReader
+from llama_index.core.schema import TextNode, NodeRelationship, Document
 
 from llama_index.core.ingestion import IngestionPipeline
-from llama_index.readers.json import JSONReader
+from llama_index.embeddings.openai import OpenAIEmbedding
 from llama_index.vector_stores.milvus import MilvusVectorStore
+
+from llama_integrations import UtteranceNodeParser, UtteranceReader
+
+
+OPENAI_MODEL="text-embedding-3-small"
 
 # We rely on LlamaIndex to be fast here.
 utterances = Path('./data/friends-corpus/friends-corpus/utterances.jsonl')
 conversations = Path("./data/friends-corpus/friends-corpus/conversations.json")
-# json_parser = JSONNodeParser()
-# pipeline = IngestionPipeline(
-#     transformations=[json_parser]
-# )
 
-class UtteranceReader(BaseReader):
-    def __init__(self, *args,
-                 ignore_transcript_notes: bool=False,
-                 ignore_alls: bool=True,
-                 ignore_other: bool=True,
-                 **kwargs):
-        super().__init__(*args, **kwargs)
-        self._ignore_transcript_notes = ignore_transcript_notes
-        self._ignore_alls = ignore_alls
-        self._ignore_other = ignore_other
-        
-    def create_document(self, id_, context, metadata):
-        return Document(id_=id_,
-                        text='\n'.join(context),
-                        metadata=metadata)
-        
-    def load_data(self, file: Path,
-                  extra_info: Optional[Dict] = None,
-                  fs: Optional[AbstractFileSystem] = None,
-    ) -> List[Document]:
-        """Parse the jsonl file into separate Documents for each conversation."""
-        # NOTE: This part doesn't scale particularly well, since it loads all of the conversations
-        # into our memory all at once. Unfortunately this is also what most LlamaIndex Readers tend to do!
-        all_convos = []
-
-        current_conversation_id = None
-        current_conversation_speakers = set()
-        current_conversation_content = []
-        with open(file) as fileobj:
-            for file_line in fileobj:
-                record = json.loads(file_line)
-                if current_conversation_id is None:   # Only done on the first line.
-                    current_conversation_id = record['conversation_id']
-
-                if current_conversation_id != record['conversation_id']:  # New conversation started.
-                    metadata = {"speakers": list(current_conversation_speakers)}  # NOTE: Could add season, episode information here.
-                    all_convos.append(
-                        self.create_document(id_=current_conversation_id,
-                                             context=current_conversation_content,
-                                             metadata=metadata)
-                    )
-
-                    # Reset information.
-                    current_conversation_id = record['conversation_id']
-                    current_conversation_speakers = set()
-                    current_conversation_content = []
-
-                speaker = record['speaker']
-                # Handling various special speaker cases.
-                # On ignores, we just skip the line altogether.
-                # Otherwise: for All and Other we give them lines but don't add them to the cast.
-                # For transcript notes we record the line differently.
-                if speaker == 'TRANSCRIPT_NOTE':
-                    if self._ignore_transcript_notes:
-                        continue
-                    else:
-                        line = f"(NOTE: {record['text']})"
-                else:
-                    if speaker == '#ALL#':
-                        if self._ignore_alls:
-                            continue
-                        else:
-                            speaker = "ALL"
-                    elif speaker == '#OTHER#':
-                        if self._ignore_other:
-                            continue
-                        else:
-                            speaker = "Other"
-                    else:  # Default case.
-                        current_conversation_speakers.add(speaker)
-                    line = f"{speaker}: {record['text']}"
-
-                if line is not None:
-                    current_conversation_content.append(line)
-
-            # End of loop. Clean up with the last conversation.
-            metadata = {"speakers": list(current_conversation_speakers)}
-            all_convos.append(
-                self.create_document(id_=current_conversation_id,
-                                        context=current_conversation_content,
-                                        metadata=metadata)
-            )
-        return all_convos
-                    
-
-def create_utterance_nodes(convofile: Path):
-    previous_node = None
-    with convofile.open() as f:
-        for line in f:
-            u = json.loads(line)
-            node_id = u['id']
-            conversation_id = u['conversation_id']
-            speaker_name = u['speaker']
-            content = u['text']
-            text = f"{speaker_name}: {content}"
-            node = TextNode(text=text, id_=node_id)
-                            
-            if previous_node is not None:
-                node.relationships[NodeRelationship.PREVIOUS] = previous_node.node_id
-                previous_node.relationships[NodeRelationship.NEXT] = node.node_id
-
-# convo = UtteranceReader().load_data(input_file=conversations)
-
-# def create_sliding_window_nodes(convofile: Path, window_size: int = 3):
-#     window = deque(maxlen=window_size)
-#     current_conversation_id = None
-#     with convofile.open() as f:
-#         for line in f:
-#             u = json.loads(line)
-#             # new conversation
-#             if u['conversation_id'] != current_conversation_id:
-#                 window.clear()
-#                 current_conversation_id = u['conversation_id']
-#             else:
-#             # add to existing conversation
-#                 pass
-            
-            
-# nodes = pipeline.run(
-#     documents=convo,
-#     show_progress=True
-# )
-
-# TODO: Adjust this URI so it works on a Docker container.
-# Milvus Lite doesn't even work on anything that's not Mac or Ubuntu. Booo!!!
-# vector_store = MilvusVectorStore(
-#     uri="http://localhost:19530", dim=1536, overwrite=True
-# )
-# storage_context = StorageContext.from_defaults(vector_store=vector_store)
-# index = VectorStoreIndex(nodes, storage_context=storage_context)
 
 if __name__ == "__main__":
-    pass
+    # Loads 67k nodes in a few seconds.
+    convos = UtteranceReader().load_data(utterances)
+    pipeline = IngestionPipeline(
+        transformations=[UtteranceNodeParser(),
+                         OpenAIEmbedding(model=OPENAI_MODEL)]
+    )
+    nodes = pipeline.run(documents=convos, show_progress=True)
+    
+    vector_store = MilvusVectorStore(
+        uri="http://localhost:19530", dim=1536, overwrite=True
+    )
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    index = VectorStoreIndex(nodes, storage_context=storage_context)
---
+++ llama_integrations.py
@@ -0,0 +1,160 @@
+"""JSON node parser."""
+import json
+from pathlib import Path
+from typing import Any, Dict, Generator, List, Optional, Sequence
+
+from llama_index.core.callbacks.base import CallbackManager
+from llama_index.core.node_parser.interface import NodeParser
+from llama_index.core.node_parser.node_utils import build_nodes_from_splits
+from llama_index.core.schema import BaseNode, MetadataMode, TextNode, NodeRelationship
+from llama_index.core.utils import get_tqdm_iterable
+
+from llama_index.core.schema import TextNode, Document
+from llama_index.core.readers.base import BaseReader
+
+
+class UtteranceReader(BaseReader):
+    def __init__(self, *args,
+                 ignore_transcript_notes: bool=False,
+                 ignore_alls: bool=True,
+                 ignore_other: bool=True,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self._ignore_transcript_notes = ignore_transcript_notes
+        self._ignore_alls = ignore_alls
+        self._ignore_other = ignore_other
+        
+    def create_document(self, id_, context, metadata):
+        return Document(id_=id_,
+                        text=json.dumps(context),
+                        metadata=metadata)
+        
+    def load_data(self, file: Path,
+                  extra_info: Optional[Dict] = None
+    ) -> List[Document]:
+        """Parse the jsonl file into separate Documents for each conversation."""
+        # NOTE: This part doesn't scale particularly well, since it loads all of the conversations
+        # into our memory all at once. Unfortunately this is also what most LlamaIndex Readers tend to do!
+        all_convos = []
+
+        current_conversation_id = None
+        current_conversation_speakers = set()
+        current_conversation_content = []
+        with open(file) as fileobj:
+            for file_line in fileobj:
+                record = json.loads(file_line)
+                if current_conversation_id is None:   # Only done on the first line.
+                    current_conversation_id = record['conversation_id']
+
+                if current_conversation_id != record['conversation_id']:  # New conversation started.
+                    metadata = {"speakers": list(current_conversation_speakers)}  # NOTE: Could add season, episode information here.
+                    all_convos.append(
+                        self.create_document(id_=current_conversation_id,
+                                             context=current_conversation_content,
+                                             metadata=metadata)
+                    )
+
+                    # Reset information.
+                    current_conversation_id = record['conversation_id']
+                    current_conversation_speakers = set()
+                    current_conversation_content = []
+
+                speaker = record['speaker']
+                # Handling various special speaker cases.
+                # On ignores, we just skip the line altogether.
+                # Otherwise: for All and Other we give them lines but don't add them to the cast.
+                # For transcript notes we record the line differently.
+                if speaker == 'TRANSCRIPT_NOTE' and self._ignore_transcript_notes:
+                    continue
+                else:
+                    if speaker == '#ALL#' and self._ignore_alls:
+                        continue
+                    elif speaker == '#OTHER#' and self._ignore_other:
+                        continue
+                    else:  # Default case.
+                        current_conversation_speakers.add(speaker)
+                    current_conversation_content.append(record)
+
+            # End of loop. Clean up with the last conversation.
+            metadata = {"speakers": list(current_conversation_speakers)}
+            all_convos.append(
+                self.create_document(id_=current_conversation_id,
+                                        context=current_conversation_content,
+                                        metadata=metadata)
+            )
+        return all_convos
+                    
+
+class UtteranceNodeParser(NodeParser):
+    """JSON node parser.
+
+    Splits a document into Nodes using custom JSON splitting logic.
+
+    Args:
+        include_metadata (bool): whether to include metadata in nodes
+        include_prev_next_rel (bool): whether to include prev/next relationships
+
+    """
+    @classmethod
+    def from_defaults(
+        cls,
+        include_metadata: bool = True,
+        include_prev_next_rel: bool = True,
+        callback_manager: Optional[CallbackManager] = None,
+    ) -> "UtteranceNodeParser":
+        callback_manager = callback_manager or CallbackManager([])
+
+        return cls(
+            include_metadata=include_metadata,
+            include_prev_next_rel=include_prev_next_rel,
+            callback_manager=callback_manager,
+        )
+
+    @classmethod
+    def class_name(cls) -> str:
+        """Get class name."""
+        return "UtteranceNodeParser"
+
+    def _parse_nodes(
+        self, nodes: Sequence[BaseNode], show_progress: bool = False, **kwargs: Any
+    ) -> List[BaseNode]:
+        all_nodes: List[BaseNode] = []
+        nodes_with_progress = get_tqdm_iterable(nodes, show_progress, "Parsing nodes")
+
+        for node in nodes_with_progress:
+            nodes = self.get_nodes_from_node(node)
+            all_nodes.extend(nodes)
+
+        return all_nodes
+
+    def make_id_func(self, id_arr: List):
+        def id_func(i: int, doc: BaseNode) -> str:
+            return id_arr[i]['id']
+        return id_func
+        
+    def get_nodes_from_node(self, node: BaseNode) -> List[TextNode]:
+        """Get nodes from document."""
+        text = node.get_content(metadata_mode=MetadataMode.NONE)
+        try:
+            data = json.loads(text)
+        except json.JSONDecodeError:
+            # Handle invalid JSON input here
+            return []
+
+        json_nodes = []
+        
+        if isinstance(data, list):
+            previous_node = None
+            for json_object in data:
+                node = TextNode(id_=json_object['id'], text=json_object['text'],
+                                metadata={'speaker': json_object['speaker'],
+                                          'conversation_id': json_object['conversation_id']})
+                json_nodes.append(node)
+            if previous_node is not None:
+                node.relationships[NodeRelationship.PREVIOUS] = previous_node.node_id
+                previous_node.relationships[NodeRelationship.NEXT] = node.node_id
+                    
+        else:
+            raise ValueError("JSON is invalid")
+
+        return json_nodes
